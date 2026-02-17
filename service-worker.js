e5fc5b7bcddbe87ddb08cbfa2f246a4d5e41b59d
choochoo2610-cmyk <choochoo2610@gmail.com>
2026-02-17 22:29:50 +0900

    Update service worker cache version and URLs

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,14 @@
-const CACHE_NAME="neurosync-cache-v1";
-const urlsToCache=["./","./index.html"];
+const CACHE="neurosync-v2";
+const urls=["./","./index.html","./manifest.json"];
 
-self.addEventListener("install",event=>{
- event.waitUntil(
-  caches.open(CACHE_NAME)
-  .then(cache=>cache.addAll(urlsToCache))
+self.addEventListener("install",e=>{
+ e.waitUntil(
+  caches.open(CACHE).then(cache=>cache.addAll(urls))
  );
 });
 
-self.addEventListener("fetch",event=>{
- event.respondWith(
-  caches.match(event.request)
-  .then(response=>response||fetch(event.request))
+self.addEventListener("fetch",e=>{
+ e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request))
  );
 });
